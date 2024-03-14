82509c5c84ee573c30711401aac1020111954a0e
Beck <somebi@gmail.com>
2024-03-14 16:59:41 +0300

    added production api address

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,3 +1,4 @@
-export const base = import.meta.env.VITE_API_BACKEND || 'http://127.0.0.1:5000'
+// For simplicity and to make frontend work in a standalone local mode, production url is kept here, should be remove later
+export const base = import.meta.env.VITE_API_BACKEND || 'https://appinio-ai-backend-ijfm4.ondigitalocean.app'
 
 export const summaryEndpoint = () => `${base}/summary`
